8f73c602c977b3e3f6fcef09b7aeb9d82415e7dc
chen <levavi.chen@gmail.com>
2024-08-08 21:08:10 +0300

    changes

--- JS/memeController.js
+++ JS/memeController.js
@@ -116,7 +116,7 @@ function renderFrame() {
     const { startPosX, startPosY, textHeight, textWidth, txt } = getTextDimensions(lines[selectedLineIdx])
     if (txt === '') return
 
-    gCtx.strokeStyle = 'red'
+    gCtx.strokeStyle = 'white'
     gCtx.strokeRect(startPosX, startPosY - textHeight, textWidth, textHeight + 10)
 
 }
@@ -223,3 +223,40 @@ function resetMemeEditor() {
 
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
 }
+
+function onUploadImg() {
+    const imgDataUrl = gElCanvas.toDataURL('image/jpeg')
+
+    function onSuccess(uploadedImgUrl) {
+        const url = encodeURIComponent(uploadedImgUrl)
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}&t=${url}`)
+    }
+
+    doUploadImg(imgDataUrl, onSuccess)
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+    const formData = new FormData()
+    formData.append('img', imgDataUrl)
+
+    const XHR = new XMLHttpRequest()
+    XHR.onreadystatechange = () => {
+        if (XHR.readyState !== XMLHttpRequest.DONE) return
+
+        if (XHR.status !== 200) return console.error('Error uploading image')
+        const { responseText: url } = XHR
+
+        console.log('Got back live url:', url)
+        onSuccess(url)
+    }
+    XHR.onerror = (req, ev) => {
+        console.error('Error connecting to server with request:', req, '\nGot response data:', ev)
+    }
+    XHR.open('POST', '//ca-upload.com/here/upload.php')
+    XHR.send(formData)
+}
+
+
+
+
+
